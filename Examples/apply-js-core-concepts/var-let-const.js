38c4f06da51383bea60b0a4bbf076e514ff7141c
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-03 19:19:36 +0600

    Concepts recap, var, let, and const

---
+++ Examples/apply-js-core-concepts/var-let-const.js
@@ -0,0 +1,8 @@
+// constant means fixed;
+// let can be change;
+let age = 11;
+age = 12;
+
+const countryName = 'Bangladesh';
+// countryName = 'Chad'; it will be shown error;
+console.log(age, countryName);
