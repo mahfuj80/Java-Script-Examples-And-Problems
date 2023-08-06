997ecfa0e6ad153e1a85f5af9910a17b42d12b6c
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-06 22:29:21 +0600

    problem 4 added

--- Problems/conceptual/problem4.js
+++ Problems/conceptual/problem4.js
@@ -8,3 +8,18 @@
  * Output: 30;
  * 
  */
+
+function isLGSeven(x) {
+    if (typeof x !== "number") {
+        return 'Please Provide me a number';
+    } else {
+        const differ = x - 7;
+        if (differ < 7) {
+            return differ;
+        } else {
+            return x * 2;
+        }
+    }
+}
+
+console.log(isLGSeven(15));
