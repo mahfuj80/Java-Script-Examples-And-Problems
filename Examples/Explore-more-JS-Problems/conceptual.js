9ee5784eb99f7d4c9c650198dc79ea2ebf5349be
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-06 21:51:22 +0600

    2nd problem added

--- Examples/Explore-more-JS-Problems/conceptual.js
+++ Examples/Explore-more-JS-Problems/conceptual.js
@@ -1,5 +1,5 @@
 // Check the number is integer or not
-
+/* 
 function isInteger(number) {
     if (typeof number !== 'number') {
         return 'please provide a number';
@@ -13,3 +13,40 @@ function isInteger(number) {
 }
 
 console.log(isInteger(500));
+ */
+/**
+ * call a function isJavaScriptFile(); give a parameter as a string. and string will be file name. and also check the file type.
+ */
+/* 
+function isJavaScriptFile(filename) {
+    if (typeof filename !== "string") {
+        return 'Please provide me a valid file name (string)'
+    } else {
+        // if (filename.toLowerCase().endsWith('.js')) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return filename.toLowerCase().endsWith('.js')
+    }
+}
+console.log(isJavaScriptFile('hello.Js'));
+ */
+
+
+function isJavaScriptFile(filename) {
+    if (typeof filename !== "string") {
+        return 'Please provide me a valid file name (string)'
+    } else {
+        // hello.world.html.js
+        const arr = filename.split('.');
+        const lastElement = arr.pop();
+        // return lastElement.toLowerCase() === 'js';
+        if (lastElement.toLowerCase() === 'js') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+console.log(isJavaScriptFile('hello.world.html.js'));
