d48b9250244b84ab0f2873296ced6d5a419f905e
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-06 22:02:27 +0600

    file changed

--- Problems/conceptual/problem2-filename.js
+++ Problems/conceptual/problem2-filename.js
@@ -1,19 +1,4 @@
-// Check the number is integer or not
-/* 
-function isInteger(number) {
-    if (typeof number !== 'number') {
-        return 'please provide a number';
-    } else {
-        if (number % 1 === 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
 
-console.log(isInteger(500));
- */
 /**
  * call a function isJavaScriptFile(); give a parameter as a string. and string will be file name. and also check the file type.
  */
